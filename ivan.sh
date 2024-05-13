907778acf05e7483fb7b2422e82f39374f17cab9
Ivansyah <ivansyahsantoso@gmail.com>
2024-05-13 15:51:38 +0700

    utama

--- ivan.sh
+++ ivan.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
+
+show_menu() {
     echo "================================="
     echo "|                               |"
     echo "|   SELAMAT DATANG, Ivansyah:D  |"
     echo "|                               |"
     echo "================================="
-    echo "| 1. Lihat file                 |"
+    echo "| 1. Cek isi file               |"
     echo "| 2. Tambahkan file             |"
+    echo "| 3. Hapus file                 |"
+    echo "| 4. Keluar                     |"
     echo "================================="
-    read -p "Masukkan pilihan: " pilihan
+
+}
+    show_menu
+while true; do
+        read -p "Masukkan pilihan: " pilihan
 
     case $pilihan in
-    1)
-        read -p "Masukkan direktori file: " dir
-        ls $dir
-        echo "File ditemukan :D"
+    1)  read -p "Masukkan direktori file: " file_to_read
+        if [ -f "$file_to_read" ]; then
+            cat $file_to_read
+        else
+            echo "file tidak ditemukan atau bukan merupakan file"
+        fi 
         ;;
-    2) read -p "Masukkan nama file baru beserta ekstensinya: " new_file
+    2)  read -p "Masukkan nama file baru beserta ekstensinya: " new_file
         touch $new_file
         echo "File baru berhasil dibuat dengan nama $new_file"
+        ;;
+    3)  read -p "Masukkan nama file yang ingin dihapus: "  path
+        rm -r $path
+        ;;
+    4)  echo "Sampai jumpa, Ivansyah :D"
+        exit 0
         ;;
     *)
         echo "Pilihan tidak valid"
         ;;
     esac 
+
+    echo 
+    show_menu
+done    
